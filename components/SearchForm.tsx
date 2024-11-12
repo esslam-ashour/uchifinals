414fa26fdc5ddd8504909b8225f43b7660546d26
Esslam Ashour <esslamhosam@gmail.com>
2024-11-12 12:40:38 -0600

    fixed enter key not working for search, small style change

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -10,10 +10,15 @@ interface SearchFormProps {
 }
 
 export default function SearchForm({ searchParams, onInputChange, onSearch }: SearchFormProps): React.ReactElement {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSearch();
+  };
+
   return (
     <div className="sticky top-0 z-10 container my-6 py-4 px-10 bg-white">
-      <form className="grid grid-rows-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-        <div className="row-span-2 md:col-span-4 lg:col-span-6">
+      <form className="grid grid-rows-2 md:grid-cols-4 lg:grid-cols-6 gap-4" onSubmit={handleSubmit}>
+        <div className="row-span-2 mb-6 md:col-span-4 lg:col-span-6">
           <Input
             id="details"
             name="details"
