d3052d8a1b1fe7ef9d465a8c2c1a2fcc3b8a6395
Esslam Ashour <esslamhosam@gmail.com>
2024-12-22 21:32:15 -0600

    Reduce navbar size for mobile

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,11 +7,11 @@ const fettefraktur = localFont({
 
 export default function NavBar() {
   return (
-    <header className="text-white py-4">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
+    <header className="text-white py-2 md:py-4">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-2 md:px-4">
         <Link href="/">
           <div className="flex flex-col items-center md:items-start">
-            <h1 className={`${fettefraktur.className} text-3xl md:text-5xl`}>
+            <h1 className={`${fettefraktur.className} text-2xl md:text-5xl`}>
               uchifinals
             </h1>
             <p className="text-xs md:text-sm mt-1 text-center md:text-left">
@@ -22,13 +22,13 @@ export default function NavBar() {
             </p>
           </div>
         </Link>
-        <nav className="mt-4 md:mt-0">
-          <ul className="flex flex-col md:flex-row gap-4 md:gap-6 text-xs md:text-base font-bold items-center">
+        <nav className="mt-2 md:mt-0">
+          <ul className="flex flex-col md:flex-row gap-2 md:gap-6 text-xs md:text-base font-bold items-center">
             <li className="text-center">
               Made by Esslam Ashour, class of 2027
             </li>
             <li>
-              <a href="mailto:esslamashour@uchicago.edu" className="text-[#800000] bg-white p-2 md:p-3 rounded-3xl font-normal hover:underline text-center block">
+              <a href="mailto:esslamashour@uchicago.edu" className="text-[#800000] bg-white p-1 md:p-3 rounded-3xl font-normal hover:underline text-center block">
                 Contact
               </a>
             </li>
