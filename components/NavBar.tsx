e00de7ca9d2f7723df4e1db6441a602c5a7d3b7a
Esslam Ashour <esslamhosam@gmail.com>
2025-01-29 20:38:36 -0600

    Slight NavBar modification

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,14 +7,21 @@ const fettefraktur = localFont({
 
 export default function NavBar() {
   return (
-    <Link href="/">
+    <div>
       <header className="text-white">
-        <div className="container mx-auto flex md:flex-row items-center justify-center">
+        <div className="container mx-auto flex md:flex-row items-center justify-between">
           <div className={`${fettefraktur.className} text-4xl md:text-5xl lg:text-6xl mt-6 md:my-8 lg:mb-2`}>
             uchifinals
           </div>
+          <div className="flex items-center space-x-4">
+            <div className="text-sm">
+              <div>Made by Esslam Ashour, Class of 2027</div>
+              <a href="https://github.com/esslam-ashour">GitHub</a><br></br>
+              <a href="https://www.linkedin.com/in/esslam-ashour/">LinkedIn</a>
+            </div>
+          </div>
         </div>
       </header>
-    </Link>
+    </div>
   )
 }
